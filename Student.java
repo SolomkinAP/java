c9873ef70a7a34968f9998dcc78fccfbf78f1f83
SolomkinAP <solomkin05@mail.ru>
2025-10-07 10:50:10 +0300

    ООП Практика 7 Задание 3

--- Student.java
+++ Student.java
@@ -1,11 +1,23 @@
-// Student.java
-public class Student extends Pupil {
-    public Student(String name) {
-        super(name);
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Student {
+    private String name;
+    private Date birthDate;
+
+    public Student(String name, Date birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String formatBirthDate(int style) {
+        DateFormat df = DateFormat.getDateInstance(style);
+        return df.format(birthDate);
     }
 
     @Override
     public String toString() {
-        return "Студент: " + name;
+        return "Студент: " + name + ", дата рождения: " + new SimpleDateFormat("dd.MM.yyyy").format(birthDate);
     }
 }
