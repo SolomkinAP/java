fb9f006f70a8d9e6b482e24c255d095d3b78a10b
SolomkinAP <solomkin05@mail.ru>
2025-10-07 10:36:34 +0300

    ООП Практика 6 Задания 1-3

--- MovablePoint.java
+++ MovablePoint.java
@@ -1,44 +1,8 @@
 public class MovablePoint implements Movable {
-    // Спецификаторы доступа package-private (~) как в UML диаграмме
-    int x;
-    int y;
-    int xSpeed;
-    int ySpeed;
-
-    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
-        this.x = x;
-        this.y = y;
-        this.xSpeed = xSpeed;
-        this.ySpeed = ySpeed;
-    }
-
-    @Override
-    public String toString() {
-        return "MovablePoint{" +
-                "x=" + x +
-                ", y=" + y +
-                ", xSpeed=" + xSpeed +
-                ", ySpeed=" + ySpeed +
-                '}';
-    }
-
-    @Override
-    public void moveUp() {
-        y -= ySpeed; // y уменьшается при движении вверх в стандартных экранных координатах
-    }
-
-    @Override
-    public void moveDown() {
-        y += ySpeed;
-    }
-
-    @Override
-    public void moveLeft() {
-        x -= xSpeed;
-    }
-
-    @Override
-    public void moveRight() {
-        x += xSpeed;
-    }
+    private int x, y, xSpeed, ySpeed;
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) { this.x=x; this.y=y; this.xSpeed=xSpeed; this.ySpeed=ySpeed; }
+    public static String numberToString(Number n){ return String.valueOf(n); }
+    public int getXSpeed(){ return xSpeed; } public int getYSpeed(){ return ySpeed; }
+    public void moveUp(){ y+=ySpeed; } public void moveDown(){ y-=ySpeed; } public void moveLeft(){ x-=xSpeed; } public void moveRight(){ x+=xSpeed; }
+    public String toString(){ return "MovablePoint[x="+x+",y="+y+"]"; }
 }
--- MovableRectangle.java
+++ MovableRectangle.java
@@ -1,56 +1,11 @@
 public class MovableRectangle implements Movable {
-    private MovablePoint topLeft;
-    private MovablePoint bottomRight;
-
-    public MovableRectangle(int x1, int y1, int x2, int y2, int xSpeed, int ySpeed) {
-        this.topLeft = new MovablePoint(x1, y1, xSpeed, ySpeed);
-        this.bottomRight = new MovablePoint(x2, y2, xSpeed, ySpeed);
-    }
-
-    // Метод для проверки одинаковой скорости, как требовалось в задании.
-    // В конструкторе точки создаются с одинаковой скоростью,
-    // поэтому проверка всегда будет успешной.
-    private boolean hasSameSpeed() {
-        return topLeft.xSpeed == bottomRight.xSpeed && topLeft.ySpeed == bottomRight.ySpeed;
-    }
-
-    @Override
-    public String toString() {
-        return "MovableRectangle{" +
-                "topLeft=" + topLeft +
-                ", bottomRight=" + bottomRight +
-                '}';
-    }
-
-    @Override
-    public void moveUp() {
-        if (hasSameSpeed()) {
-            topLeft.moveUp();
-            bottomRight.moveUp();
-        }
-    }
-
-    @Override
-    public void moveDown() {
-        if (hasSameSpeed()) {
-            topLeft.moveDown();
-            bottomRight.moveDown();
-        }
-    }
-
-    @Override
-    public void moveLeft() {
-        if (hasSameSpeed()) {
-            topLeft.moveLeft();
-            bottomRight.moveLeft();
-        }
-    }
-
-    @Override
-    public void moveRight() {
-        if (hasSameSpeed()) {
-            topLeft.moveRight();
-            bottomRight.moveRight();
-        }
-    }
+    private MovablePoint topLeft,bottomRight;
+    public MovableRectangle(int x1,int y1,int x2,int y2,int xs,int ys){
+        topLeft=new MovablePoint(x1,y1,xs,ys); bottomRight=new MovablePoint(x2,y2,xs,ys); }
+    public boolean speedTest(){ return topLeft.getXSpeed()==bottomRight.getXSpeed()&&topLeft.getYSpeed()==bottomRight.getYSpeed(); }
+    public void moveUp(){ topLeft.moveUp(); bottomRight.moveUp(); }
+    public void moveDown(){ topLeft.moveDown(); bottomRight.moveDown(); }
+    public void moveLeft(){ topLeft.moveLeft(); bottomRight.moveLeft(); }
+    public void moveRight(){ topLeft.moveRight(); bottomRight.moveRight(); }
+    public String toString(){ return "Rect[topLeft="+topLeft+",bottomRight="+bottomRight+"]"; }
 }
